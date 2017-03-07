c9352d8396212a8714d2216ad885a96a8f08d143
Johannes Zillmann <jz@datameer.com>
2017-03-07 08:47:50 +0100

    [WIP] improve TOC parsing

--- src/javascript/models/transformations/DetectTOC.jsx
+++ src/javascript/models/transformations/DetectTOC.jsx
@@ -16,21 +16,24 @@ export default class DetectTOC extends ToPdfBlockViewTransformation {
 
     transform(parseResult:ParseResult) {
         const {mostUsedDistance} = parseResult.globals;
-        var foundTocPages = 0;
+        const tocPages = [];
         const maxPagesToEvaluate = Math.min(20, parseResult.content.length);
         const textCombiner = new TextItemCombiner({
             mostUsedDistance: mostUsedDistance
         });
 
-
+        var lastLevel = 0;
+        const itemLeveler = new ItemLeveler();
         parseResult.content.slice(0, maxPagesToEvaluate).forEach(page => {
             var linesCount = 0;
             var linesWithDigitsCount = 0;
             var lineItemsWithDigits = [];
+            const unknownBlocks = new Set();
             var headlineBlock;
             page.blocks.forEach(block => {
                 var blockHasLinesWithDigits = false;
                 const itemsGroupedByY = textCombiner.combine(block.textItems).textItems;
+                var lastLineTextWithoutNumber;
                 itemsGroupedByY.forEach(lineItem => {
                     linesCount++
                     var lineText = lineItem.text.replace(/\./g, '').trim();
@@ -41,26 +44,37 @@ export default class DetectTOC extends ToPdfBlockViewTransformation {
                     }
                     lineText = lineText.trim();
                     if (endsWithDigit) {
+                        if (lastLineTextWithoutNumber) { // 2-line item ?
+                            lineText = lastLineTextWithoutNumber + ' ' + lineText;
+                            lastLineTextWithoutNumber = null;
+                        }
                         linesWithDigitsCount++;
                         blockHasLinesWithDigits = true;
                         lineItemsWithDigits.push(new TextItem({
                             ...lineItem,
                             text: lineText
                         }));
+                    } else {
+                        lastLineTextWithoutNumber = lineText;
                     }
                 });
-                if (!headlineBlock && !blockHasLinesWithDigits) {
-                    headlineBlock = block;
+                if (!blockHasLinesWithDigits) {
+                    if (!headlineBlock) {
+                        headlineBlock = block;
+                    } else {
+                        unknownBlocks.add(block);
+                    }
                 }
             });
 
             if (linesWithDigitsCount * 100 / linesCount > 75) {
-                foundTocPages++;
+                tocPages.push(page.index + 1);
                 const newBlocks = [];
                 page.blocks.forEach((block) => {
-                    block.annotation = REMOVED_ANNOTATION;
+                    if (!unknownBlocks.has(block)) {
+                        block.annotation = REMOVED_ANNOTATION;
+                    }
                     newBlocks.push(block);
-
                     if (block === headlineBlock) {
                         newBlocks.push(new PdfBlock({
                             textItems: textCombiner.combine(block.textItems).textItems,
@@ -69,6 +83,8 @@ export default class DetectTOC extends ToPdfBlockViewTransformation {
                         }));
                     }
                 });
+                // lastLevel = processLevels(lineItemsWithDigits, lastLevel);
+                itemLeveler.level(lineItemsWithDigits);
                 newBlocks.push(new PdfBlock({
                     textItems: lineItemsWithDigits,
                     type: TOC_BLOCK,
@@ -80,8 +96,104 @@ export default class DetectTOC extends ToPdfBlockViewTransformation {
 
         return new ParseResult({
             ...parseResult,
-            messages: ['Detected ' + foundTocPages + ' table of content pages']
+            globals: {
+                ...parseResult.globals,
+                tocPages: tocPages
+
+            },
+            messages: ['Detected ' + tocPages.length + ' table of content pages']
         });
     }
 
+}
+
+
+class ItemLeveler {
+    constructor() {
+        this.levelByMethod = null;
+        this.uniqueFonts = [];
+        this.headlines = [];
+    }
+
+    level(lineItemsWithDigits) {
+        if (!this.levelByMethod) {
+            const uniqueX = this.calculateUniqueX(lineItemsWithDigits);
+            if (uniqueX.length > 1) {
+                this.levelByMethod = this.levelByXDiff;
+            } else {
+                const uniqueFonts = this.calculateUniqueFonts(lineItemsWithDigits);
+                if (uniqueFonts.length > 1) {
+                    this.uniqueFonts = uniqueFonts;
+                    this.levelByMethod = this.levelByFont;
+                } else {
+                    this.levelByMethod = this.levelToZero;
+                }
+            }
+        }
+        this.levelByMethod(lineItemsWithDigits);
+    }
+
+    levelByXDiff(lineItemsWithDigits) {
+        const uniqueX = this.calculateUniqueX(lineItemsWithDigits);
+        lineItemsWithDigits.forEach(item => {
+            const level = uniqueX.indexOf(item.x);
+            this.headlines.push(new Headline({
+                level: level,
+                text: item.text
+            }));
+            item.text = ' '.repeat(level * 3) + '- ' + item.text;
+        });
+    }
+
+    levelByFont(lineItemsWithDigits) {
+        lineItemsWithDigits.forEach(item => {
+            const level = this.uniqueFonts.indexOf(item.font);
+            this.headlines.push(new Headline({
+                level: level,
+                text: item.text
+            }));
+            item.text = ' '.repeat(level * 3) + '- ' + item.text;
+        });
+    }
+
+    levelToZero(lineItemsWithDigits) {
+        lineItemsWithDigits.forEach(item => {
+            const level = 0;
+            this.headlines.push(new Headline({
+                level: level,
+                text: item.text
+            }));
+            item.text = ' '.repeat(level * 3) + '- ' + item.text;
+        });
+    }
+
+    calculateUniqueX(lineItemsWithDigits) {
+        var uniqueX = lineItemsWithDigits.reduce(function(uniquesArray, lineItem) {
+            if (uniquesArray.indexOf(lineItem.x) < 0) uniquesArray.push(lineItem.x);
+            return uniquesArray;
+        }, []);
+
+        uniqueX.sort((a, b) => {
+            return a - b
+        });
+
+        return uniqueX;
+    }
+
+    calculateUniqueFonts(lineItemsWithDigits) {
+        var uniqueFont = lineItemsWithDigits.reduce(function(uniquesArray, lineItem) {
+            if (uniquesArray.indexOf(lineItem.font) < 0) uniquesArray.push(lineItem.font);
+            return uniquesArray;
+        }, []);
+
+        return uniqueFont;
+    }
+
+}
+
+class Headline {
+    constructor(options) {
+        this.level = options.level;
+        this.text = options.text;
+    }
 }
