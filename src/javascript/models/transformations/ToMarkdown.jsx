2d6d889fbb38382683863cb2dccf71d7d061dc71
Laura Lee <laura.x.y.lee@gmail.com>
2019-05-28 18:16:28 +0800

    Concatenate text in the same paragraph

--- src/javascript/models/transformations/ToMarkdown.jsx
+++ src/javascript/models/transformations/ToMarkdown.jsx
@@ -17,8 +17,38 @@ export default class ToMarkdown extends Transformation {
         parseResult.pages.forEach(page => {
             var text = '';
             page.items.forEach(block => {
-                text += block.text + '\n';
+                if (block.category == "TOC") {
+                    // Don't concatenate words in Table of Contents
+                    var concatWords = block.text
+                } else {
+                    // Concatenate words in same block by removing newlines
+                    var concatText = block.text.replace(/(\r\n|\n|\r)/gm, " ");
+
+                    // Concat words broken up by newline
+                    var concatWords = concatText.split("- ").join("");
+
+                    // // TODO Find '- ', remove it and check if it's a word
+                    // // If it's a word, remove '- '
+                    // // If not, replace it with '-'
+                    // if (concatText.includes("- ")) {
+                    //     var splitText = concatText.split("- ")
+                    //
+                    //     var i;
+                    //     for (i = 1; i < splitText.length; i++) {
+                    //         var precedingText = splitText[i-1].split(" ").slice(-1)[0]
+                    //         var succeedingText = splitText[i].split(" ")[0]
+                    //         var word = precedingText + succeedingText
+                    //     }
+                    // }
+                }
+
+                if (block.category == "CODE") {
+                    // Remove all code blocks. Assume documents have no code blocks
+                    var concatWords = concatWords.split("`").join("");
+                }
+                text += concatWords + '\n\n';
             });
+
             page.items = [text];
         });
         return new ParseResult({
