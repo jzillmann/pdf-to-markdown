ff8ce8c7d4aa99ae8b734d0ff67262f2e9007a7f
ricktjwong <rickwtj@gmail.com>
2019-06-26 11:23:45 +0800

    Add option for recursive conversion of PDFs     - Use minimalist CLI package for specification of parameters

--- README.md
+++ README.md
@@ -21,7 +21,25 @@ Use the [issue tracker](https://github.com/jzillmann/pdf-to-markdown/issues) and
 - ```npm run watch``` Continuously build the project
 - ```open build/index.html``` Open the build project in your default browser
 - ```npm run release``` Build production version
-- ```npm run deploy``` Build production version & move it to the github pages fodler
+- ```npm run deploy``` Build production version & move it to the github pages foldler
+- ```npm run prepare``` Babelify necessary files and package into the `dist/` directory
+
+#### CLI tool
+Instructions to run:
+<pre>
+$ cd [project_folder]
+$ npm run prepare
+$ node dist/pdf2md-cli.js --inputFolderPath=[your input folder path] --outputFolderPath=[your output folder path] --recursive=[true or false]
+</pre>
+If you are converting recursively on a large number of files you might encounter the error "Allocation failed - JavaScript heap out of memory”. Instead, run the command
+<pre>
+$ node --max-old-space-size=4096 index.js --inputFolderPath=[your input folder path] --outputFolderPath=[your output folder path] --recursive=[1 or 0]
+</pre>
+
+Options:
+1. Input folder path (should exist)
+2. Output folder path (should exist)
+3. Recursive - convert all PDFs for folders within folders
 
 #### Release
 - Increase version in package.json
--- src/javascript/pdf2md-cli.js
+++ src/javascript/pdf2md-cli.js
@@ -4,32 +4,84 @@
 
 const pdf2md = require('./pdf2md')
 
-const fs = require('fs')
+
+const fs = require('fs');
 const path = require('path')
+var argv = require('minimist')(process.argv.slice(2));
 
-const [ node, appName, inputFile, outputPath ] = process.argv
-
-if (!inputFile) {
-  console.log(`Usage: ${appName} ${inputFile} [${outputPath}]`)
-  process.exit(1)
+if (!argv["inputFolderPath"]) {
+    console.log("Please specify inputFolderPath")
+} else if (!argv["outputFolderPath"]) {
+    console.log("Please specify outputFolderPath")
+} else if (!argv["recursive"]) {
+    console.log("Please specify recursive equals 1 or 0")
+} else {
+    const folderPath = argv["inputFolderPath"]
+    const outputPath = argv["outputFolderPath"] 
+    const recursive = parseInt(argv["recursive"])
+    run(folderPath, outputPath, recursive)
 }
 
-// If outputPath specified, supply callbacks to log progress
-const callbacks = outputPath && {}
+function run(folderPath, outputPath, recursive=1) {
+  var outputArray = getPaths(folderPath)
+  var filePaths = outputArray[0]
+  var folderPaths = outputArray[1]
+  var allFolderPaths = folderPaths
+  if (recursive) {
+      while (allFolderPaths.length != 0) {
+          var nextFolderPaths = []
+          allFolderPaths.forEach(folderPath => {
+              outputArray = getPaths(folderPath)
+              filePaths = filePaths.concat(outputArray[0])
+              nextFolderPaths = nextFolderPaths.concat(outputArray[1])
+              folderPaths = folderPaths.concat(outputArray[1])
+          });
+          allFolderPaths = nextFolderPaths
+      }
+  }
+  var allOutputPaths = filePaths.map(x => {
+      return outputPath + x.split(folderPath)[1].split('.')[0]
+  })
+  allOutputPaths.forEach(outputPath => {
+      outputPath = outputPath.split('/').slice(0, -1).join('/')
+      if (!fs.existsSync(outputPath)) {
+          fs.mkdirSync(outputPath, { recursive: true })
+      }
+  })
+  createMarkdownFiles(filePaths, allOutputPaths)
+}
 
-const pdfBuffer = fs.readFileSync(path.resolve(inputFile))
-pdf2md(pdfBuffer, callbacks)
-  .then(text => {
-    if (outputPath) {
-      const outputFile = outputPath + "/output.md"
-      console.log(`Writing to ${outputFile}...`)
-      fs.writeFileSync(path.resolve(outputFile), text)
-      console.log('Done.')
-    } else {
-      console.log(text)
-    }
+function getPaths(folderPath) {
+  var filePaths = []
+  var folderPaths = []
+  var directoryItems = fs.readdirSync(folderPath)
+  directoryItems.forEach(directoryItem => {
+      const isDirectory = fs.lstatSync(folderPath + '/' + directoryItem).isDirectory()
+      if (isDirectory) {
+          folderPaths.push(folderPath + '/' + directoryItem)
+      } 
+      if (directoryItem.split('.').pop() == 'pdf') {
+          filePaths.push(folderPath + '/' + directoryItem)
+      }
+  });
+  return [filePaths, folderPaths]
+}
+
+function createMarkdownFiles(filePaths, allOutputPaths) {
+  // If outputPath specified, supply callbacks to log progress
+  filePaths.forEach(async function(filePath, i) {
+      const callbacks = allOutputPaths[i] && {}
+      const pdfBuffer = fs.readFileSync(filePath)
+      pdf2md(pdfBuffer, callbacks)
+        .then(text => {
+          let outputFile = allOutputPaths[i] + '.md'
+          console.log(`Writing to ${outputFile}...`)
+          fs.writeFileSync(path.resolve(outputFile), text)
+          console.log('Done.')
+        })
+        .catch(err => {
+          console.error(err)
+          process.exit(1)
+        })
   })
-  .catch(err => {
-    console.error(err)
-    process.exit(1)
-  })
+}
