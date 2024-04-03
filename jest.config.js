16e5a62951f53671f68103c6a436894aedeecf9e
Johannes Zillmann <jzillmann@googlemail.com>
2024-04-02 18:19:20 -0600

    Fix jest setup deprecation

--- jest.config.js
+++ jest.config.js
@@ -2,19 +2,11 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['./test'],
-  transform: { '\\.ts$': ['ts-jest'] },
   testRegex: '(/test/.*|(\\.|/))(test)\\.(ts)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
     'src/(.*)': '<rootDir>/src/$1',
     'test/(.*)': '<rootDir>/test/$1',
   },
-  globals: {
-    'ts-jest': {
-      tsconfig: {
-        // allow js in typescript
-        allowJs: true,
-      },
-    },
-  },
+  transform: { '\\.ts$': ['ts-jest'] },
 };
